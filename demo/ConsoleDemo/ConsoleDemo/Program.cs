8d57aa01b8832e5f90df4239edf5c2aef0b26ccb
Jaya Aakash <aakashunique0609@gmail.com>
2024-02-22 14:57:40 +0530

    C# code

---
+++ demo/ConsoleDemo/ConsoleDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my first demo application !!....");
+            Console.WriteLine("enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("where you reside:");
+            string place = Console.ReadLine();
+            Console.Read();
+
+        }
+    }
+}
