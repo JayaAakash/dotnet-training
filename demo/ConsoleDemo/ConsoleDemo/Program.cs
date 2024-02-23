54bde4a8e3e08274891db24ca042c0ee0ac0ebdc
Jaya Aakash <aakashunique0609@gmail.com>
2024-02-23 18:09:08 +0530

    program

--- demo/ConsoleDemo/ConsoleDemo/Program.cs
+++ demo/ConsoleDemo/ConsoleDemo/Program.cs
@@ -11,8 +11,33 @@ namespace ConsoleDemo
             string name = Console.ReadLine();
             Console.WriteLine("where you reside:");
             string place = Console.ReadLine();
+            Cricket cricket = new Cricket();
+            cricket.team();
+            
             Console.Read();
 
         }
     }
+    class Cricket
+    {
+        public void team()
+        {
+            
+            Console.WriteLine("enter your fav player name to see score virat/rohit/d/honi/pandya/rahul");
+            string inputString = Console.ReadLine();
+            if (inputString == "virat")
+                Console.WriteLine("virat scored 100");
+            else if (inputString == "dhoni")
+                Console.WriteLine("dhoni scored 103");
+            else if (inputString == "rohit")
+                Console.WriteLine("rohit scored 109");
+            else if (inputString == "pandya")
+                Console.WriteLine("pandya scored 77");
+            else if (inputString == "rahul")
+                Console.WriteLine("rahul scored 88");
+            else
+                Console.WriteLine("invalid player");
+        }
+
+        }
 }
