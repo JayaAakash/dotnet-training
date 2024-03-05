c76e30f76d15e70b0e7cf95d71e9cdc55fbf4107
Jaya Aakash <aakashunique0609@gmail.com>
2024-03-05 09:26:14 +0530

    assignments

--- Assignment/C#/Assingments/Assingments/atm.cs
+++ Assignment/C#/Assingments/Assingments/atm.cs
@@ -84,6 +84,7 @@ namespace Asssignments
 
 
             account.ShowData();
+            Console.ReadLine();
         }
     }
 }
---
+++ Assignment/C#/Assingments/Assingments/salesdetails.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingments
+{
+
+    class Saledetails
+    {
+        private int SalesNo;
+        private int ProductNo;
+        private double Price;
+        private DateTime DateofSale;
+        private int Qty;
+        private double TotalAmount;
+
+        public Saledetails(int salesNo, int productNo, double price, int qty, DateTime dateofSale)
+        {
+            SalesNo = salesNo;
+            ProductNo = productNo;
+            Price = price;
+            Qty = qty;
+            DateofSale = dateofSale;
+            Sales(); 
+        }
+
+        private void Sales()
+        {
+            TotalAmount = Qty * Price;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Sales No: {SalesNo}");
+            Console.WriteLine($"Product No: {ProductNo}");
+            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Date of Sale: {DateofSale}");
+            Console.WriteLine($"Qty: {Qty}");
+            Console.WriteLine($"Total Amount: {TotalAmount}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saledetails sale = new Saledetails(9299, 01, 20000, 10, DateTime.Now);
+
+            sale.ShowData();
+            Console.ReadLine();
+        }
+    }
+}
