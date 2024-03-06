e0547775736ffc8211cf060817d8eb40d1ffedaa
Jaya Aakash <aakashunique0609@gmail.com>
2024-03-06 10:36:17 +0530

    test

---
+++ code base test/C#/code base test/ConsoleApp1/ConsoleApp1/grads.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace code_base_test
+{
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public double Grade { get; set; }
+
+        public abstract bool IsPassed(double grade);
+    }
+
+    class Undergraduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70;
+        }
+    }
+
+    class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80;
+        }
+    }
+
+   
+}
---
+++ code base test/C#/code base test/ConsoleApp1/ConsoleApp1/product.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace code_base_test
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Enter details for product {i}:");
+                Console.Write("Product ID: ");
+                int productId = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Product Name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                Product product = new Product
+                {
+                    ProductId = productId,
+                    ProductName = productName,
+                    Price = price
+                };
+
+                products.Add(product);
+            }
+
+            products.Sort((x, y) => x.Price.CompareTo(y.Price));
+
+            Console.WriteLine("Sorted Products:");
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Price: {product.Price}");
+
+            }
+
+            Console.WriteLine("==============Student grades program=============");
+
+            Undergraduate undergraduate = new Undergraduate
+            {
+                Name = "Banu Rekha",
+                StudentId = 0001,
+                Grade = 72
+            };
+
+            Console.WriteLine($"{undergraduate.Name} (ID: {undergraduate.StudentId}) passed the course: {undergraduate.IsPassed(undergraduate.Grade)}");
+
+            // Test with Graduate
+            Graduate graduate = new Graduate
+            {
+                Name = "Aakash",
+                StudentId = 0002,
+                Grade = 72
+            };
+
+            Console.WriteLine($"{graduate.Name} (ID: {graduate.StudentId}) passed the course: {graduate.IsPassed(graduate.Grade)}");
+        
+
+        Console.ReadLine();
+        }
+    }
+}
